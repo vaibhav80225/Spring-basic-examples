89152d014ffa13e139cc9b3ebf0d8c6244a4eaf9
vaibhav80225 <vaibhav80225@gmail.com>
2022-07-17 13:17:13 +0530

    Creating and Object by using component annotation that is managed by spring container

--- ioc/src/main/java/com/itvaib/ioc/IocDemoApplication.java
+++ ioc/src/main/java/com/itvaib/ioc/IocDemoApplication.java
@@ -2,27 +2,41 @@ package com.itvaib.ioc;
 
 import com.itvaib.ioc.service.CoachService;
 import com.itvaib.ioc.service.impl.BaseBallCoach;
-import com.itvaib.ioc.service.impl.TrachCoach;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class IocDemoApplication {
 
+
+	static Logger logger = LoggerFactory.getLogger(IocDemoApplication.class.getName());
+
+	// Craeting an application context object which hold
+	// all the detail relted to spring container
 	private static ApplicationContext applicationContext;
-	@Autowired
-	private BaseBallCoach baseBallCoach;
 
 	public static void main(String[] args) {
+		// Assign the application context
 		applicationContext = SpringApplication.run(IocDemoApplication.class, args);
-		CoachService coachService = applicationContext.getBean("baseBallCoach", BaseBallCoach.class);
-		CoachService coachService1 = new TrachCoach();
-		System.out.println(coachService.getDailyWorkOut());
-		System.out.println(coachService1.getDailyWorkOut());
+		displayAllBeansName();
+		// Here baseBallCoach is the bean id that we alredy define in BaseBallCoach Service
+		CoachService coachService = applicationContext.getBean("myBaseBallCoach", BaseBallCoach.class);
+		logger.info(coachService.getDailyWorkOut());
 
+	}
+
+	/**
+	 * Getting all the beans that is craeted by Spring
+	 * as you can see in console log baseBallCoach also there
+	 */
+	public static void displayAllBeansName(){
+		String [] beanNames = applicationContext.getBeanDefinitionNames();
+		for(String beanName : beanNames){
+			logger.info("Bean Name: "+ beanName);
+		}
 	}
 
 }
--- ioc/src/main/java/com/itvaib/ioc/service/impl/TrackCoach.java
+++ ioc/src/main/java/com/itvaib/ioc/service/impl/TrackCoach.java
@@ -2,15 +2,14 @@ package com.itvaib.ioc.service.impl;
 
 import com.itvaib.ioc.service.CoachService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 // Creating an Object which is managed by spring container
-// here I provided the bean id explicitly
-@Component("myBaseBallCoach")
-public class BaseBallCoach implements CoachService {
-
+// Here I am not passing bean id so spring automatically create one for us
+// and bydefualt spring follow camelcase for bean id ex - TrackCoach = trackCoach
+@Component
+public class TrackCoach implements CoachService {
     @Override
     public String getDailyWorkOut() {
-        return "Spend 30 Minute on batting practice";
+        return "Run Hard 5 KM";
     }
 }
